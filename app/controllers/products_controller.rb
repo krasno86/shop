4df017ad1d09dfc09e669b2565461950898e3818
oleg <krasno1986@gmail.com>
2019-08-08 01:22:24 +0300

    refactor delivery controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show]
 
   def show
-    @product_info = Page.get_full_product_content(@product)
+    @product_info = Page.get_full_product_content(@product).html_safe
   end
 
   private
