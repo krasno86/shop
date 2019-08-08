6bcf1a650102e85c8d6635a37ad9e992bf954677
oleg <krasno1986@gmail.com>
2019-08-08 13:32:19 +0300

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Product.create(
 ContactInfo.create(email: 'bla@gmail.com', phone: '0123456789', user_id: merchant.id)
 
 
-Page.create(page_type: 'contact', html: '<body><p>fdvdv</p><p class="email">email:</p><p class="phone">phone number:</p><body>', user_id: merchant.id)
+Page.create(page_type: 'contact', html: '<body><p class="email">email:</p><p class="phone">phone number:</p><body>', user_id: merchant.id)
 
 Page.create(page_type: 'product', html: "<body><p><img alt=\"\" src=\"/uploads/ckeditor/pictures/3/content_Xiaomi-Mi-Mix-2s-white-1.png\" style=\"width: 300px; height: 300px;\" /></p>\r\n\r\n<p>Диагональ экрана: 5.99&quot;;</p>\r\n\r\n<p>Разрешение экрана: 2160x1080;</p>\r\n\r\n<p>Камера: 12 Мп + 12 Мп;</p>\r\n\r\n<p>Количество ядер: 8;</p>\r\n\r\n<p>Оперативная память: 6 Гб;</p>\r\n\r\n<p>Внутренняя память: 64 Гб;</p>\r\n\r\n<h3 class=\"price\">&nbsp;</h3>\r\n\r\n<form class=\"button_to\" method=\"get\" action=\"/orders/new\"><input class=\"btn btn-warning\" type=\"submit\" value=\"Create Order\"></form></body>", user_id: merchant.id)
 
